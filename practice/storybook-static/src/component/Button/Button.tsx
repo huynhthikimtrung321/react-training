922ccd41b8d5d37b1ba9de084efbbb57b634b566
huynhthikimtrung321 <beohuynh321@gmail.com>
2024-05-27 13:49:36 +0700

    fix(naming): update naming

--- practice/storybook-static/src/component/Button/Button.tsx
+++ practice/storybook-static/src/component/Button/Button.tsx
@@ -1,10 +1,14 @@
-import './button.css';
+import './Button.css';
 
 interface ButtonProps {
+    /**
+   * Button contents
+   */
+    label: string;
   /**
    * Is this the principal call to action on the page?
    */
-  primary?: boolean;
+  variant?: 'primary' | 'secondary';
   /**
    * What background color to use
    */
@@ -13,10 +17,6 @@ interface ButtonProps {
    * How large should the button be?
    */
   size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
   /**
    * Optional click handler
    */
@@ -27,17 +27,17 @@ interface ButtonProps {
  * Primary UI component for user interaction
  */
 export const Button = ({
-  primary = false,
+  variant = 'primary', // default to 'primary' if not specified
   size = 'medium',
   backgroundColor,
   label,
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = variant === 'primary' ? 'button--primary' : 'button--secondary';
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={['button', `button--${size}`, mode].join(' ')}
       style={{ backgroundColor }}
       {...props}
     >
