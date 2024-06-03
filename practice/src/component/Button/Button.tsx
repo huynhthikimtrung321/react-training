1f01b66fda4bbbf59a4f2c4be57f98cc1414db03
huynhthikimtrung321 <beohuynh321@gmail.com>
2024-06-03 17:11:26 +0700

    feat(component): update button

--- practice/src/component/Button/Button.tsx
+++ practice/src/component/Button/Button.tsx
@@ -1,7 +1,10 @@
+import { MouseEvent } from 'react';
+
 import './Button.css';
 
 interface ButtonProps {
   label?: string;
+  value?: string;
   hover?: 'color' | 'underline';
   color?: 'default' | 'tertiary';
   variant?: 'ghost' | 'outline';
@@ -9,11 +12,12 @@ interface ButtonProps {
   size?: 'small' | 'medium';
   isActive?: boolean;
   icon?: React.ReactNode;
-  onClick?: () => void;
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
 }
 
 export const Button = ({
   variant = 'ghost',
+  value = '',
   hover,
   color = 'default',
   size = 'medium',
@@ -21,6 +25,7 @@ export const Button = ({
   label,
   isActive = false,
   icon,
+  onClick,
 }: ButtonProps) => {
   const mode = variant === 'ghost' ? 'button--ghost' : 'button--outline';
   const hoverClass = hover ? `hover-${hover}` : '';
@@ -29,6 +34,8 @@ export const Button = ({
   return (
     <button
       type="button"
+      value={value}
+      onClick={onClick}
       className={[
         'button-border-default',
         hoverClass,
