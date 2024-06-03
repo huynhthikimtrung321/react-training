6b22d2243b8ce99f9ee0ea2ee905c84abc87323b
huynhthikimtrung321 <beohuynh321@gmail.com>
2024-06-03 15:54:45 +0700

    feat(component): implement handleInputChange

--- practice/src/component/CardHeader/CardHeader.tsx
+++ practice/src/component/CardHeader/CardHeader.tsx
@@ -1,21 +1,43 @@
+import { Item } from '../../type';
 import { Checkbox, Input } from '..';
 
 import './CardHeader.css';
+import { Dispatch, useState, ChangeEvent } from 'react';
 
-export const CardHeader = () => {
+interface CardHeaderProps {
+  items: Item[];
+  setItems: Dispatch<React.SetStateAction<Item[]>>;
+}
+
+export const CardHeader = ({ items, setItems }: CardHeaderProps) => {
+  const [value, setValue] = useState('');
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleClickAll = () => {
+    setIsChecked(!isChecked);
+
+    const newItems = [...items];
+    newItems.forEach(item => (item.isCompleted = !isChecked));
+    setItems(newItems);
+  };
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
   return (
     <div className="card-header wrapper">
       <Checkbox
         className="card-select-all"
         variant="select-all"
-        isChecked={false}
-        onChange={() => {}}
+        isChecked={isChecked}
+        onChange={handleClickAll}
       />
       <Input
-        value=""
+        value={value}
         type="text"
         placeholder="What needs to be done?"
-        onChange={() => {}}
+        onChange={handleInputChange}
         onKeyDown={() => {}}
       />
     </div>
