806e0da7317b6664fcf7baca86f2172575251053
huynhthikimtrung321 <beohuynh321@gmail.com>
2024-06-03 17:13:06 +0700

    feat(component): update main.tsx

--- practice/src/App.tsx
+++ practice/src/App.tsx
@@ -1,25 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import { CardTodo } from './component/CardTodo/CardTodo';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <main>
+      <CardTodo />
+    </main>
+  );
+};
 
-export default App
+export default App;
