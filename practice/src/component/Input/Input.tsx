4efb5e87ec125b9e7e5e40acf10bfb8568401b5e
huynhthikimtrung321 <beohuynh321@gmail.com>
2024-06-05 16:17:16 +0700

    feat(component): update CardHeader

--- practice/src/component/Input/Input.tsx
+++ practice/src/component/Input/Input.tsx
@@ -1,20 +1,24 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, KeyboardEvent } from 'react';
 import './Input.css';
 
 interface InputProps {
   value: string;
-  placeholder?: string;
   type: string;
+  placeholder?: string;
+  autofocus?: boolean;
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
-  onKeyDown: () => void;
+  onKeyDown: (event: KeyboardEvent<HTMLInputElement>) => void;
+  onBlur?: () => void;
 }
 
 export const Input = ({
   value,
   placeholder,
   type,
+  autofocus = false,
   onChange,
   onKeyDown,
+  onBlur,
 }: InputProps) => {
   return (
     <input
@@ -22,8 +26,10 @@ export const Input = ({
       type={type}
       placeholder={placeholder}
       value={value}
+      autoFocus={autofocus}
       onChange={onChange}
       onKeyDown={onKeyDown}
+      onBlur={onBlur}
     />
   );
 };
