9560f1bae8ffd191593023c589e7415acb280d7d
huynhthikimtrung321 <beohuynh321@gmail.com>
2024-06-05 16:20:51 +0700

    feat(component): update CardItem

--- practice/src/component/Button/Button.tsx
+++ practice/src/component/Button/Button.tsx
@@ -1,7 +1,10 @@
+import { MouseEvent } from 'react';
+
 import './Button.css';
 
 interface ButtonProps {
   label?: string;
+  value?: string;
   hover?: 'color' | 'underline';
   color?: 'default' | 'tertiary';
   variant?: 'ghost' | 'outline';
@@ -9,7 +12,8 @@ interface ButtonProps {
   size?: 'small' | 'medium';
   isActive?: boolean;
   icon?: React.ReactNode;
-  onClick?: () => void;
+  className?: string;
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
 }
 
 export const Button = ({
@@ -19,8 +23,11 @@ export const Button = ({
   size = 'medium',
   borderColor,
   label,
+  value,
   isActive = false,
   icon,
+  className,
+  onClick,
 }: ButtonProps) => {
   const mode = variant === 'ghost' ? 'button--ghost' : 'button--outline';
   const hoverClass = hover ? `hover-${hover}` : '';
@@ -29,17 +36,20 @@ export const Button = ({
   return (
     <button
       type="button"
+      value={value}
       className={[
         'button-border-default',
         hoverClass,
         `button--${size}`,
         'background-color-transparent',
         'hover-cursor-pointer',
+        className,
         colorClass,
         activeClass,
         mode,
       ].join(' ')}
       style={{ borderColor }}
+      onClick={onClick}
     >
       {icon ? icon : label}
     </button>
